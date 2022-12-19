28d63df1a3c9fc67bb371e93e5fcc5d9b7ab0cac
choi <choi@DESKTOP-5O4R01C>
2022-12-19 16:08:50 +0900

    dfdddd

--- dash/src/main/java/com/sp/app/HomeController.java
+++ dash/src/main/java/com/sp/app/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
 		Calendar cal = Calendar.getInstance();
 		
 		int year = cal.get(Calendar.YEAR);
-		int month = cal.get(Calendar.MONTH);
+		int month = cal.get(Calendar.MONTH)+1;
 		int day = cal.get(Calendar.DATE);
 		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
 		
