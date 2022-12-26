882a961d935a022dd1ae0c86fa3c9c4415229c06
Kang Yeong Woo <rkdduddn8812@naver.com>
2022-12-26 15:34:02 +0900

    승인 처리

--- dash/src/main/java/com/sp/app/approval/ApprovalController.java
+++ dash/src/main/java/com/sp/app/approval/ApprovalController.java
@@ -1,14 +1,31 @@
 package com.sp.app.approval;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sp.app.employee.Employee;
+import com.sp.app.employee.EmployeeService;
+import com.sp.app.employee.SessionInfo;
 
 @Controller("approval.approvalController")
 @RequestMapping(value = "/approval/*")
 public class ApprovalController {
-	@RequestMapping(value = "main")
-	public String main() {
-		return ".approval.main";
-	}
+	@Autowired
+	   private EmployeeService service2;
+	   
+	   @RequestMapping(value = "main")
+	   public String main(HttpSession session, Model model) throws Exception {
+	      
+	      SessionInfo info = (SessionInfo)session.getAttribute("employee");
+	      Employee dto = service2.readMain(info.getEmpNo());
+	      
+	      model.addAttribute("dto", dto);
+	      
+	      return ".approval.main";
+	   }
 
 }
