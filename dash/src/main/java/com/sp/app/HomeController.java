d5ce9a435546afb1745dc3db3bc4ab2832e0066c
Kang Yeong Woo <rkdduddn8812@naver.com>
2022-12-26 15:00:56 +0900

    홈컨트롤러

--- dash/src/main/java/com/sp/app/HomeController.java
+++ dash/src/main/java/com/sp/app/HomeController.java
@@ -23,97 +23,102 @@ import com.sp.app.employee.SessionInfo;
 
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private AnalysisService service;
-	
-	@Autowired
-	private EmployeeService service2;
+   
+   @Autowired
+   private AnalysisService service;
+   
+   @Autowired
+   private EmployeeService service2;
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, HttpSession session, Model model) throws Exception {
-		Calendar cal = Calendar.getInstance();
-		
-		int year = cal.get(Calendar.YEAR);
-		int month = cal.get(Calendar.MONTH)+1;
-		int day = cal.get(Calendar.DATE);
-		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-		
-		String korDayOfWeek = "";
-		switch(dayOfWeek) {
-			case 1:
-				korDayOfWeek = "일";
-				break;
-			case 2:
-				korDayOfWeek = "월";
-				break;
-			case 3:
-				korDayOfWeek = "화";
-				break;
-			case 4:
-				korDayOfWeek = "수";
-				break;
-			case 5:
-				korDayOfWeek = "목";
-				break;
-			case 6:
-				korDayOfWeek = "금";
-				break;
-			case 7:
-				korDayOfWeek = "토";
-				break;
-		}
-		
-		String msg = year + "." + month + "." + day + "(" + korDayOfWeek + ")";
-		String s = "즐거운 " + korDayOfWeek + "요일입니다.";
-		
-		String totalSales = service.totalSales();
-		int rentalCount = service.rentalCount();
-		int newCustomerCount = service.newCustomerCount();
-		int repairCount = service.repairCount();
-		int damageCount = service.damageCount();
-		
-		List<Analysis> usageRankList = service.usageRankList();
-		
-		SessionInfo info = (SessionInfo)session.getAttribute("employee");
-		Employee dto = service2.readMain(info.getEmpNo());
-		
-		model.addAttribute("msg", msg);
-		model.addAttribute("dayOfWeek", s);
-		model.addAttribute("totalSales", totalSales);
-		model.addAttribute("rentalCount", rentalCount);
-		model.addAttribute("newCustomerCount", newCustomerCount);
-		model.addAttribute("repairCount", repairCount);
-		model.addAttribute("damageCount", damageCount);
-		model.addAttribute("usageRankList", usageRankList);
-		
-		model.addAttribute("dto", dto);
-		
-		return ".mainLayout";
-	}
-	
-	@RequestMapping("zoonSalesAnalysis")
-	@ResponseBody
-	public Map<String, Object> salesRankList() throws Exception {
-		Map<String, Object> model = new HashMap<String, Object>();
+   @RequestMapping(value = "/", method = RequestMethod.GET)
+   public String home(Locale locale, HttpSession session, Model model) throws Exception {
+      Calendar cal = Calendar.getInstance();
+      
+      int year = cal.get(Calendar.YEAR);
+      int month = cal.get(Calendar.MONTH)+1;
+      int day = cal.get(Calendar.DATE);
+      int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+      
+      String korDayOfWeek = "";
+      switch(dayOfWeek) {
+         case 1:
+            korDayOfWeek = "일";
+            break;
+         case 2:
+            korDayOfWeek = "월";
+            break;
+         case 3:
+            korDayOfWeek = "화";
+            break;
+         case 4:
+            korDayOfWeek = "수";
+            break;
+         case 5:
+            korDayOfWeek = "목";
+            break;
+         case 6:
+            korDayOfWeek = "금";
+            break;
+         case 7:
+            korDayOfWeek = "토";
+            break;
+      }
+      
+      String msg = year + "." + month + "." + day + "(" + korDayOfWeek + ")";
+      String s = "즐거운 " + korDayOfWeek + "요일입니다.";
+      
+      String totalSales = service.totalSales();
+      int rentalCount = service.rentalCount();
+      int newCustomerCount = service.newCustomerCount();
+      int repairCount = service.repairCount();
+      int damageCount = service.damageCount();
+      
+      List<Analysis> usageRankList = service.usageRankList();
+      
+      SessionInfo info = (SessionInfo)session.getAttribute("employee");
+      
+      if(info == null) {
+         return "redirect:/employee/login";
+      }    
+      
+      Employee dto = service2.readMain(info.getEmpNo());
+      
+      model.addAttribute("msg", msg);
+      model.addAttribute("dayOfWeek", s);
+      model.addAttribute("totalSales", totalSales);
+      model.addAttribute("rentalCount", rentalCount);
+      model.addAttribute("newCustomerCount", newCustomerCount);
+      model.addAttribute("repairCount", repairCount);
+      model.addAttribute("damageCount", damageCount);
+      model.addAttribute("usageRankList", usageRankList);
+      
+      model.addAttribute("dto", dto);
+      
+      return ".mainLayout";
+   }
+   
+   @RequestMapping("zoonSalesAnalysis")
+   @ResponseBody
+   public Map<String, Object> salesRankList() throws Exception {
+      Map<String, Object> model = new HashMap<String, Object>();
 
-		List<Analysis> salesRankList = service.salesRankList();
+      List<Analysis> salesRankList = service.salesRankList();
 
-		model.put("salesRankList", salesRankList);
+      model.put("salesRankList", salesRankList);
 
-		return model;
-	}
-	
-	@RequestMapping("zoonUsageCountAnalysis")
-	@ResponseBody
-	public Map<String, Object> lastDayUsageCount() throws Exception {
-		Map<String, Object> model = new HashMap<String, Object>();
+      return model;
+   }
+   
+   @RequestMapping("zoonUsageCountAnalysis")
+   @ResponseBody
+   public Map<String, Object> lastDayUsageCount() throws Exception {
+      Map<String, Object> model = new HashMap<String, Object>();
 
-		Analysis lastDayUsageCount = service.lastDayUsageCount();
+      Analysis lastDayUsageCount = service.lastDayUsageCount();
 
-		model.put("lastDayUsageCount", lastDayUsageCount);
+      model.put("lastDayUsageCount", lastDayUsageCount);
 
-		return model;
-	}
-	
+      return model;
+   }
+   
 }
