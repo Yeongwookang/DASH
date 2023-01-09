aa367a996cdb5d526257385f09f65021eff892cc
song-yujin <song-yujin@localhost>
2023-01-09 16:33:56 +0900

    dddd

--- dash/src/main/java/com/sp/app/marker/MarkerController.java
+++ dash/src/main/java/com/sp/app/marker/MarkerController.java
@@ -3,7 +3,7 @@ package com.sp.app.marker;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("map.mapController")
+@Controller("map.markerController")
 @RequestMapping("/map/*")
 public class MarkerController {
 
