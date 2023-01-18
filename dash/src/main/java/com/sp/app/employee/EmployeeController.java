afe7c4f005e01da1a5744a4214bc144fe1a0df90
choi <choi@DESKTOP-5O4R01C>
2023-01-19 03:30:07 +0900

    로그인

--- dash/src/main/java/com/sp/app/employee/EmployeeController.java
+++ dash/src/main/java/com/sp/app/employee/EmployeeController.java
@@ -3,7 +3,6 @@ package com.sp.app.employee;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,13 +18,9 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService service;
 	
-	@Autowired
-	private BCryptPasswordEncoder bcrypt;
-	
 	@RequestMapping(value = "login")
-	public String loginForm(Model model) {
-		String pwd = bcrypt.encode("java$!");
-		model.addAttribute("pwd", pwd);
+	public String loginForm() {
+		
 		return "employee/login";
 	}
 	
